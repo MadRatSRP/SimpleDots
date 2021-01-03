3347f431565966e40aa740429808461b8da7fa42
MadRat96 <mischa.alpeew@yandex.ru>
2021-01-03 19:05:53 +0300

    project:
    module "app":
    -Updated MainActivity's layout
    
    module "DotsIndicator":
    -Removed unused resources
    -Added "colors" list
    -Refactored DotsIndicator class a bit

--- DotsIndicator/src/main/java/com/make/dots/dotsindicator/DotsIndicator.kt
+++ DotsIndicator/src/main/java/com/make/dots/dotsindicator/DotsIndicator.kt
@@ -1,24 +1,24 @@
 package com.make.dots.dotsindicator
 
-import android.content.Context
-import android.widget.LinearLayout
-import androidx.annotation.DrawableRes
-import androidx.viewpager.widget.ViewPager.OnPageChangeListener
-import android.database.DataSetObserver
-import androidx.viewpager.widget.ViewPager
-import android.view.Gravity
 import android.annotation.TargetApi
+import android.content.Context
+import android.database.DataSetObserver
 import android.os.Build
 import android.util.AttributeSet
+import android.view.Gravity
 import android.view.View
+import android.widget.LinearLayout
+import androidx.annotation.DrawableRes
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 
 class DotsIndicator : LinearLayout {
     lateinit var mViewpager: ViewPager
     private var mIndicatorMargin = -1
     private var mIndicatorWidth = -1
     private var mIndicatorHeight = -1
-    private var mIndicatorBackgroundResId = R.drawable.ic_dot_darkgrey
-    private var mIndicatorUnselectedBackgroundResId = R.drawable.ic_dot_lightgrey
+    private var mIndicatorBackgroundResId = R.drawable.dot_selected
+    private var mIndicatorUnselectedBackgroundResId = R.drawable.dot_unselected
     private var mLastPosition = -1
 
     private val mInternalPageChangeListener = object : OnPageChangeListener {
@@ -48,20 +48,28 @@ class DotsIndicator : LinearLayout {
         }
     }
 
-    constructor(context: Context) : super(context) {
+    constructor(context: Context)
+        : super(context) {
+        
         init(context, null)
     }
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+    constructor(context: Context, attrs: AttributeSet)
+        : super(context, attrs) {
+        
         init(context, attrs)
     }
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int)
+        : super(context, attrs, defStyleAttr) {
+        
         init(context, attrs)
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int)
+        : super(context, attrs, defStyleAttr, defStyleRes) {
+        
         init(context, attrs)
     }
 
@@ -71,26 +79,68 @@ class DotsIndicator : LinearLayout {
     }
 
     private fun handleTypedArray(context: Context, attrs: AttributeSet?) {
-        if (attrs == null) return
+        if (attrs == null) {
+            return
+        }
+        
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.DotsIndicator)
-        mIndicatorWidth = typedArray.getDimensionPixelSize(R.styleable.DotsIndicator_dot_width, -1)
-        mIndicatorHeight = typedArray.getDimensionPixelSize(R.styleable.DotsIndicator_dot_height, -1)
-        mIndicatorMargin = typedArray.getDimensionPixelSize(R.styleable.DotsIndicator_dot_margin, -1)
-        mIndicatorBackgroundResId = typedArray.getResourceId(R.styleable.DotsIndicator_dot_drawable, R.drawable.ic_dot_darkgrey)
-        mIndicatorUnselectedBackgroundResId = typedArray.getResourceId(R.styleable.DotsIndicator_dot_drawable_unselected, mIndicatorBackgroundResId)
-        val orientation = typedArray.getInt(R.styleable.DotsIndicator_dot_orientation, -1)
-        setOrientation(if (orientation == LinearLayout.VERTICAL) LinearLayout.VERTICAL else LinearLayout.HORIZONTAL)
-        val gravity = typedArray.getInt(R.styleable.DotsIndicator_dot_gravity, -1)
-        setGravity(if (gravity >= 0) gravity else Gravity.CENTER)
-        typedArray.recycle()
+        with(typedArray) {
+            mIndicatorWidth = getDimensionPixelSize(R.styleable.DotsIndicator_dot_width, -1)
+            mIndicatorHeight = getDimensionPixelSize(R.styleable.DotsIndicator_dot_height, -1)
+            mIndicatorMargin = getDimensionPixelSize(R.styleable.DotsIndicator_dot_margin, -1)
+            mIndicatorBackgroundResId = getResourceId(R.styleable.DotsIndicator_dot_drawable,
+                R.drawable.dot_selected)
+            mIndicatorUnselectedBackgroundResId = getResourceId(
+                R.styleable.DotsIndicator_dot_drawable_unselected, mIndicatorBackgroundResId)
+    
+            val orientation = getInt(R.styleable.DotsIndicator_dot_orientation, -1)
+            setOrientation(if (orientation == VERTICAL) {
+                VERTICAL
+            } else {
+                HORIZONTAL
+            })
+    
+            val gravity = getInt(R.styleable.DotsIndicator_dot_gravity, -1)
+            setGravity(if (gravity >= 0) {
+                gravity
+            } else {
+                Gravity.CENTER
+            })
+    
+            recycle()
+        }
     }
 
     private fun checkIndicatorConfig() {
-        mIndicatorWidth = if (mIndicatorWidth < 0) dpTopx(DEFAULT_INDICATOR_WIDTH.toFloat()) else mIndicatorWidth
-        mIndicatorHeight = if (mIndicatorHeight < 0) dpTopx(DEFAULT_INDICATOR_WIDTH.toFloat()) else mIndicatorHeight
-        mIndicatorMargin = if (mIndicatorMargin < 0) dpTopx(DEFAULT_INDICATOR_WIDTH.toFloat()) else mIndicatorMargin
-        mIndicatorBackgroundResId = if (mIndicatorBackgroundResId == 0) R.drawable.ic_dot_lightgrey else mIndicatorBackgroundResId
-        mIndicatorUnselectedBackgroundResId = if (mIndicatorUnselectedBackgroundResId == 0) mIndicatorBackgroundResId else mIndicatorUnselectedBackgroundResId
+        mIndicatorWidth = if (mIndicatorWidth < 0) {
+            convertDpToPx(DEFAULT_INDICATOR_WIDTH.toFloat())
+        } else {
+            mIndicatorWidth
+        }
+        
+        mIndicatorHeight = if (mIndicatorHeight < 0) {
+            convertDpToPx(DEFAULT_INDICATOR_WIDTH.toFloat())
+        } else {
+            mIndicatorHeight
+        }
+        
+        mIndicatorMargin = if (mIndicatorMargin < 0) {
+            convertDpToPx(DEFAULT_INDICATOR_WIDTH.toFloat())
+        } else {
+            mIndicatorMargin
+        }
+        
+        mIndicatorBackgroundResId = if (mIndicatorBackgroundResId == 0) {
+            R.drawable.dot_unselected
+        } else {
+            mIndicatorBackgroundResId
+        }
+        
+        mIndicatorUnselectedBackgroundResId = if (mIndicatorUnselectedBackgroundResId == 0) {
+            mIndicatorBackgroundResId
+        } else {
+            mIndicatorUnselectedBackgroundResId
+        }
     }
 
     fun setViewPager(viewPager: ViewPager) {
@@ -106,12 +156,19 @@ class DotsIndicator : LinearLayout {
 
     private fun createIndicators() {
         removeAllViews()
+        
         val count = mViewpager.adapter?.count ?: 0
-        if (count <= 0) return
+        if (count <= 0) {
+            return
+        }
+        
         val currentItem = mViewpager.currentItem
         for (i in 0 until count) {
-            if (currentItem == i) addIndicator(mIndicatorBackgroundResId)
-            else addIndicator(mIndicatorUnselectedBackgroundResId)
+            if (currentItem == i) {
+                addIndicator(mIndicatorBackgroundResId)
+            } else {
+                addIndicator(mIndicatorUnselectedBackgroundResId)
+            }
         }
     }
 
@@ -120,15 +177,17 @@ class DotsIndicator : LinearLayout {
         indicator.setBackgroundResource(backgroundDrawableId)
         addView(indicator, mIndicatorWidth, mIndicatorHeight)
         
-        val lp = indicator.layoutParams as LayoutParams
-        lp.leftMargin = mIndicatorMargin
-        lp.rightMargin = mIndicatorMargin
-        lp.topMargin = mIndicatorMargin
-        lp.bottomMargin = mIndicatorMargin
-        indicator.layoutParams = lp
+        val layoutParams = indicator.layoutParams as LayoutParams
+        indicator.layoutParams = layoutParams.apply {
+            leftMargin = mIndicatorMargin
+            rightMargin = mIndicatorMargin
+            topMargin = mIndicatorMargin
+            bottomMargin = mIndicatorMargin
+        }
     }
 
-    fun dpTopx(dpValue: Float): Int = (dpValue * resources.displayMetrics.density + 0.5f).toInt()
+    private fun convertDpToPx(dpValue: Float): Int
+        = (dpValue * resources.displayMetrics.density + 0.5f).toInt()
 
     companion object {
         private const val DEFAULT_INDICATOR_WIDTH = 5
